27668eb2e1b9ab8ad29e754e20a642a01c40b2e6
Rishu123-png <jaswarrishu642@gmail.com>
2025-11-15 22:01:15 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
-export const app = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBSWzs19870cWmGxd9-kJsKOOs755jyuU0",
   authDomain: "school-attendence-system-9090.firebaseapp.com",
   databaseURL: "https://school-attendence-system-9090-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -9,4 +11,9 @@ export const app = initializeApp({
   storageBucket: "school-attendence-system-9090.firebasestorage.app",
   messagingSenderId: "728832169882",
   appId: "1:728832169882:web:b335869779e73ab8c20c23"
-});
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);

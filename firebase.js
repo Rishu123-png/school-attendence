3acb90a8cb6fc05cbf7bcb2d06bfc813c210807a
Rishu123-png <jaswarrishu642@gmail.com>
2025-11-15 21:34:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSWzs19870cWmGxd9-kJsKOOs755jyuU0",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

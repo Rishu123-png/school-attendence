c34c865665338b424e739586586e7282480a945f
Rishu123-png <jaswarrishu642@gmail.com>
2025-11-16 20:22:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,15 +4,18 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSWzs19870cWmGxd9-kJsKOOs755jyuU0",
-  authDomain: "school-attendence-system-9090.firebaseapp.com",
-  databaseURL: "https://school-attendence-system-9090-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "school-attendence-system-9090",
-  storageBucket: "school-attendence-system-9090.firebasestorage.app",
-  messagingSenderId: "728832169882",
-  appId: "1:728832169882:web:b335869779e73ab8c20c23"
+  apiKey: "AIzaSyBhAgQutvqEVE8hGPG360MD72sGmSHrmTw",
+  authDomain: "school-attendace-d87e4.firebaseapp.com",
+  databaseURL: "https://school-attendace-d87e4-default-rtdb.firebaseio.com",
+  projectId: "school-attendace-d87e4",
+  storageBucket: "school-attendace-d87e4.appspot.com",
+  messagingSenderId: "969792838746",
+  appId: "1:969792838746:web:bc7d770fc4d9b951d8b06e"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
